f984a5e9d3842e82b475566fe3c6da16c731073d
Castro25 <lordcastro25@gmail.com>
2023-12-02 17:33:01 +0100

    asd

---
+++ car_sounds/audi_rs6.sound/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'ea825_game.dat151.rel',
+    'ea825_sounds.dat54.rel',
+    'sfx/dlc_ea825/**/*'
+}
+
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea825'
