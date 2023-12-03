2a68d29fa1baa8310ffe7ab33cbb34ce02220df3
Castro25 <lordcastro25@gmail.com>
2023-12-03 16:29:53 +0100

    asd

---
+++ [car]/ford_mustang/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Your Name'
+description 'Your resource description'
+version '1.0.0'
+
+-- Specify the files required for your resource
+files {
+    'carvariations.meta',
+    'dlctext.meta',
+    'handling.meta',
+    'vehicles.meta'
+}
+
+-- Specify the data files
+data_file 'CARVARIATIONS_META_FILE' 'carvariations.meta'
+data_file 'DLCTEXT_META_FILE' 'dlctext.meta'
+data_file 'HANDLING_META_FILE' 'handling.meta'
+data_file 'VEHICLES_META_FILE' 'vehicles.meta'
